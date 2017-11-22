cd15e22db80330a4bc3408a04f5be17a7060d139
Gabriel Falcão <gabriel@nacaolivre.org>
2017-11-21 19:46:31 -0500

    New release: 1.0.2

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"A simple, flexible and testable active-record powered by SQLAlchemy."
+
 
 import ast
 import os
 from setuptools import setup, find_packages
 
 
-local_file = lambda *f: \
-    open(os.path.join(os.path.dirname(__file__), *f)).read()
+local_file = lambda *f: open(os.path.join(os.path.dirname(__file__), *f)).read()
 
 
 class VersionFinder(ast.NodeVisitor):
@@ -35,10 +36,26 @@ def read_requirements():
     return local_file('requirements.txt').splitlines()
 
 
+def read_readme():
+    """Read README content.
+    If the README.rst file does not exist yet
+    (this is the case when not releasing)
+    only the short description is returned.
+    """
+    try:
+        return local_file('README.rst')
+    except IOError:
+        return __doc__
+
+
 setup(
     name='chemist',
     version=read_version(),
-    description='',
+    description=read_readme(),
+    author='Gabriel Falcao',
+    author_email='gabriel@nacaolivre.org',
+    maintainer='Gabriel Falcao',
+    maintainer_email='gabriel@nacaolivre.org',
     entry_points={
         'console_scripts': ['chemist = chemist.console:entrypoint'],
     },
@@ -46,7 +63,10 @@ setup(
     author_email='gabriel@nacaolivre.org',
     packages=find_packages(exclude=['*tests*']),
     install_requires=read_requirements(),
+    test_suite='nose.collector',
     include_package_data=True,
+    description=__doc__,
+    long_description=read_readme(),
     package_data={
         'chemist': ' '.join([
             '*.cfg',
@@ -55,5 +75,19 @@ setup(
             '*.txt',
         ]),
     },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ]
     zip_safe=False,
 )
